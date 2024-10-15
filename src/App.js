f7795168f5ec384a5eb0b157bd71fb006e2c774f
elizabethvilloria <lizvilloria@gmail.com>
2024-10-15 21:59:55 +0200

    Set up basic structure for orderform

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Post from './Post';
+import OrderForm from './OrderForm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="/order/:id" element={<OrderForm />} />
       </Routes>
     </Router>
   );
