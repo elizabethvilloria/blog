632a85677d29c0c4465e5f46bdb857b52a7e67d8
elizabethvilloria <lizvilloria@gmail.com>
2024-11-06 23:19:11 +0100

    Fixed error wrong page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-const LoadingSpinner = () => (
-  <div className="loading-spinner">
-    <div className="spinner"></div>
-  </div>
-);
-
-export default LoadingSpinner;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
